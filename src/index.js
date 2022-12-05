a7fbff765367f121e361fd09f8e1460eab833981
Jacek Korzemski <wrp40.000@wp.pl>
2022-12-05 19:02:04 +0100

    basic BE for app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,115 @@
+const http = require("http");
+const app = require("./app");
+
+app.set("port", 3001);
+const server = http.createServer(app);
+
 const dbConnect = require("./db/dbConnect");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("./db/userModel");
 
 dbConnect();
+
+app.get("/test", (request, response) => {
+  response.send("get request is working");
+  response.end();
+});
+
+app.post("/test", (request, response) => {
+  response.send("post request is working");
+  response.end();
+});
+
+app.post("/register", (request, response) => {
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+app.post("/login", (request, response) => {
+  User.findOne({ email: request.body.email })
+
+    .then((user) => {
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        .then((passwordCheck) => {
+          if (!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          }
+
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          response.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
+          });
+        })
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
+        e,
+      });
+    });
+});
+
+// TODO: [
+//   create Review model
+//   create add review endpoint
+//   create get reviews by video id endpoint
+// ]
+
+app.get("/get-reviews", (request, response) => {
+  response.json({ message: "You are free to access me anytime" });
+});
+
+app.get("/post-review", (request, response) => {
+  response.json({ message: "You are authorized to access me" });
+});
+
+server.listen(3001);
